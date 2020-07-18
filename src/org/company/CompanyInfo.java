533c40303439251a8980a5cf8b5db2e54c7342a7
pushparaj117 <u_pushparaj@yahoo.co.in>
2020-07-18 14:55:35 +0530

    completed

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,56 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		System.out.println("company name");
+
+	}
+	
+	private void companyName(int id) {
+		System.out.println("company id:"+id);
+
+	}
+	private void companyName(int id, int yoj) {
+		System.out.println("Company id:"+ id);
+		
+	System.out.println("Year of Joining:"+ yoj);
+
+	}
+	
+	private void companyName(String name, long mobile) {
+		System.out.println("Client name:"+name);
+		System.out.println("mobile number:"+mobile);
+
+	}
+	private void companyName(long mobile, float rating) {
+		System.out.println("Company rating:"+rating);
+		System.out.println("mobile number:"+mobile);
+
+	}
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		c.companyName();
+		c.companyName(123);
+		c.companyName(435, 2020);
+		c.companyName(9876598765l, 4.5f);
+		c.companyName("Sathya", 89898989l);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
